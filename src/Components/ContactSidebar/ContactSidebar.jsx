87d2123e3b5e3eba808c654c0add4abd19bde3f5
SantiagoVillegas <Santivillegas473@gmail.com>
2026-01-25 15:09:53 -0300

    agregacion de iconos laterales del aside, arreglo de la pantalla sobresalida, y cambio de colores
    Co-authored-by: morasergio05 <morasergio05@users.noreply.github.com>

--- src/Components/ContactSidebar/ContactSidebar.jsx
+++ src/Components/ContactSidebar/ContactSidebar.jsx
@@ -10,8 +10,9 @@ export default function ContactSidebar() {
         <div className='contact-sidebar'>
             <div className='home-screen-icon-menu'>
                 <h1>WhatsApp</h1>
-                <div>
-                    <span><i className="bi bi-three-dots-vertical"></i></span>
+                <div className='home-screen-icon-menu-icons'>
+                    <span><i className="bi bi-chat-left-text" title="Nuevo chat"></i></span>
+                    <span><i className="bi bi-three-dots-vertical" title="Menu"></i></span>
                 </div>
             </div>
             <SearchContactform />
--- src/Screens/HomeScreen/HomeScreen.jsx
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -10,8 +10,20 @@ export default function HomeScreen(props) {
     return (
         <div className="home-screen">
             <div className="home-screen-content">
+                <aside className='home-screen-content-menu'>
+                    <div className='home-screen-content-menu-icons-social'>
+                        <span className='span-content-icon-chat'><i class="bi bi-chat-left-text-fill " title="Chats" ></i></span>
+                        <span className='span-content-icon-status' ><i className="bi bi-plus-circle" title="Estados"></i></span>
+                        <span className='span-content-icon -diffusion'><i className="bi bi-chat-dots" title="Canales"></i></span>
+                        <span className='span-content-icon-comunity'><i className="bi bi-people" title="Difusion"></i></span>
+                    </div>
+                    <div className='home-screen-content-menu-icon-settings'>
+                        <span className='span-content-icon-gallery'><i className="bi bi-images" title="Fotos y Videos"></i></span>
+                        <span className='span-content-icon-settings'><i className="bi bi-gear"></i></span>
+                        <span className='span-content-icon-profile'><i className="bi bi-person"></i></span>
+                    </div>
+                </aside>
                 <div className="sidebar-container">
-
                     <ContactSidebar />
                 </div>
                 <div className="placeholder-chat">
