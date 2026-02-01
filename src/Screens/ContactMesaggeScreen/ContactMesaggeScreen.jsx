59965edb9494aa49cc65b260f328f3ce6c616c4f
SantiagoVillegas <Santivillegas473@gmail.com>
2026-02-01 16:00:58 -0300

    version 1.0.1 arreglos de detalle, eliminacion de duplicidad de codigo

--- src/Screens/ContactMesaggeScreen/ContactMesaggeScreen.jsx
+++ src/Screens/ContactMesaggeScreen/ContactMesaggeScreen.jsx
@@ -7,6 +7,7 @@ import { ContactDetailContext } from '../../Contexts/ContactDetailContext';
 import { ContactContexts } from '../../Contexts/ContactContexts';
 import NewMessageForm from '../../Components/NewMessagesForm/NewMessageForm';
 import ContactSidebar from '../../Components/ContactSidebar/ContactSidebar';
+import AsideMenu from '../../Components/AsideMenu/AsideMenu';
 /* contact_id: 1,
         contact_name: 'Laura',
         contact_avatar: 'https://www.seoptimer.com/storage/images/2014/08/no-con-la-mascota.jpg',
@@ -27,19 +28,7 @@ export default function ContactMesaggeScreen() {
     return (
         <div className="contact-message-screen-wrapper">
             <div className="home-screen-content">
-                <aside className='home-screen-content-menu'>
-                    <div className='home-screen-content-menu-icons-social'>
-                        <span className='span-content-icon-chat'><i class="bi bi-chat-left-text-fill"></i></span>
-                        <span className='span-content-icon-status'><i className="bi bi-plus-circle"></i></span>
-                        <span className='span-content-icon-diffusion'><i className="bi bi-chat-dots"></i></span>
-                        <span className='span-content-icon-comunity'><i className="bi bi-people"></i></span>
-                    </div>
-                    <div className='home-screen-content-menu-icon-settings'>
-                        <span className='span-content-icon-gallery'><i className="bi bi-images"></i></span>
-                        <span className='span-content-icon-settings'><i className="bi bi-gear"></i></span>
-                        <span className='span-content-icon-profile'><i className="bi bi-person"></i></span>
-                    </div>
-                </aside>
+                <AsideMenu />
                 <div className="sidebar-container">
 
                     <ContactSidebar />
